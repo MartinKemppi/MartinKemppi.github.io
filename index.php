cfeee788f1fb7ab2994fa168d2604b53b33fb7fb
Martin Kemppi <dusperin1234@gmail.com>
2024-05-24 10:26:07 +0300

    index ja css on lisatud
    index & css on loodud

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Git hub page</title>
+    <link rel="stylesheet" href="minustiil.css">
+</head>
+<body>
+<header>
+
+    <footer>
+        <p>&copy; 2024 Martin Kemppi</p>
+    </footer>
+</body>
+</html>
