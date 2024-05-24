0c3f989f062f9f95381171af6460eec1bd3c65de
Martin Kemppi <dusperin1234@gmail.com>
2024-05-24 10:28:07 +0300

    2 section oskused
    2 section oskused on loodud ja kirjeldatud

--- index.php
+++ index.php
@@ -23,6 +23,26 @@
         <h2>Martin Kemppi</h2>
         <p>Noorem tarkvaraarendaja</p>
     </section>
+    <section id="skills">
+        <h2>Oskused</h2>
+        <h3>Keeleoskused</h3>
+        <ul>
+            <li>Eesti keel - B2</li>
+            <li>Inglise keel - B2</li>
+            <li>Vene keel - C2</li>
+            <li>Soome keel - B1</li>
+            <li>Saksa keel - A1</li>
+        </ul>
+        <h3>Arvutioskused</h3>
+        <ul>
+            <li>Virtualiseerimine: Oracle VM VirtualBox</li>
+            <li>Kontoritarkvara: MS Word, MS Excel, MS Powerpoint, MS Visio, MS Access, MS Project</li>
+            <li>Programmeerimiskeeled: Python, SQL, C#, HTML, CSS, Javascript, PHP, .NET</li>
+            <li>Graafika, küljendus, multimeedia: Adobe Photoshop, Gimp, Inkscape, Adobe Illustrator, Adobe After Effects, Adobe Audition</li>
+            <li>Võrgundus: Cisco Packet Tracer</li>
+            <li>Operatsioonisüsteemid: Linux, Windows</li>
+        </ul>
+    </section>
 </main>
     <footer>
         <p>&copy; 2024 Martin Kemppi</p>
